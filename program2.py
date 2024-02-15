6b31f1edc9d79956d50f0816c0791d03c6299e2c
Ankitvermagec <112874033+Ankitvermagec@users.noreply.github.com>
2024-02-15 15:26:57 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -1,5 +1,5 @@
 class Solution(object):
-  def romanToInt(s: str) -> int:
+  def romanToInt(sels,s: str) -> int:
     r_int = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     final_result = 0
     
