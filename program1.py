c36bdb3896c21cfac2e20752a072135a9707d398
Ankitvermagec <112874033+Ankitvermagec@users.noreply.github.com>
2024-02-15 15:21:12 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -1,12 +1,26 @@
 class Solution(object):
-    def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        pass
-    
+  def isValid(self,s: str) -> bool:
+    arr = []
+    b_ = {')': '(', '}': '{', ']': '['}
 
+    for char in s:
+        if char in b_.values():
+            arr.append(char)
+        elif char in b_.keys():
+            if not arr or arr.pop() != b_[char]:
+                return False
+        else:
+            
+            return False
+
+    
+    return not arr
+
+obj=Solution()
+
+print(obj.isValid("()"))      
+print(obj.isValid("()[]{}"))   
+print(obj.isValid("(]"))       
 
 
   
--- program2.py
+++ program2.py
@@ -1,7 +1,18 @@
 class Solution(object):
-    def romanToInt(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        pass
+  def romanToInt(s: str) -> int:
+    r_int = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    final_result = 0
+    
+    for i in range(len(s)):
+        if i < len(s) - 1 and r_int[s[i]] < r_int[s[i+1]]:
+            final_result -= r_int[s[i]]
+        else:
+            final_result += r_int[s[i]]
+    
+    return final_result
+
+obj=Solution()
+
+print(obj.romanToInt("III"))       
+print(obj.romanToInt("LVIII"))     
+print(obj.romanToInt("MCMXCIV"))   
